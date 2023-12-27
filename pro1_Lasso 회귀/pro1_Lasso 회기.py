d6ce928c720c0c7081bb556849cbeebb3ef107df
yoonsuk <yoonsuk.choi93@gmail.com>
2023-12-27 23:54:15 +0900

    Lasso 회기

--- pro1_Lasso 회귀/pro1_Lasso 회기.py
+++ pro1_Lasso 회귀/pro1_Lasso 회기.py
@@ -6,3 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import matplotlib.pyplot as plt
+
+# 뉴스 데이터 업로드
+# 거래날짜/거래량/전체 토픽발생량/토픽1/토픽2/.../토픽50
+
+# 표준화
+
+# 분류
+
+# 회기분석
+
+# 알파값
